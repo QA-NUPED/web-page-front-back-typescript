9e5576cfe7a2eba218a3c591775667622d232db9
Gleidison Nascimento <gleidison.nascimento@stone.com.br>
2023-08-07 14:14:37 -0300

    feat(web-project): criando o from de cadastro e configurando o retorno do back

--- back-end/routes/user.ts
+++ back-end/routes/user.ts
@@ -3,9 +3,12 @@ import express from 'express';
 //Controllers
 import { store } from '../controllers/User';
 
+//Validation
+import { userStoryValidate } from '../validations/user-validate';
+
 
 const router = express.Router();
 
-router.post('/create', store)
+router.post('/create', userStoryValidate, store);
 
 export default router
--- front-end/src/create.ts
+++ front-end/src/create.ts
@@ -2,15 +2,34 @@ import http from "./helpers/http";
 
 function create() {
     return {
-        create: async function() {
-            try{
-                const {data} = await http.post('/user/create');
+        user: {
+            firstName:'Nome',
+            lastName:'Sobrenome',
+            email:'email',
+            password:'senha',
+        },
+        createUser: async function () {
+            try {
+                const { data } = await http.post('/user/create', this.user);
                 console.log(data);
-            }catch(error){
-                console.log(error);
+            } catch (error: any) {
+                const errors = error.response?.data?.errors;
+                if (errors) {
+                    errors.forEach((element: any) => {
+                        const elementValidation = document.querySelector(
+                            `#error-${element.path}`
+                        ) as HTMLSpanElement;
+                        elementValidation.innerHTML = element.msg;
+
+                        setTimeout(() => {
+                            elementValidation.innerHTML = "";
+                        }, 3000);
+                    });
+                }
+                console.log();
             }
-        }
-    }
+        },
+    };
 }
 
 export default create;
